a6de21895f9328eea17920f521b907f54bfa6afe
gamic <gamic.nl@gmail.com>
2024-09-03 15:15:04 -0400

    se corrigieron los queries EDA

--- 02_EDA_QUERIES/001_production_by_month.sql
+++ 02_EDA_QUERIES/001_production_by_month.sql
@@ -1,15 +1,12 @@
-SELECT TO_CHAR(c.date, 'month') as MES,
+SELECT TO_CHAR(o.created_at, 'month') as MES,
 		i.type as item,
 		SUM (oi.order_quantity) as quantity
 FROM orders AS o LEFT JOIN order_item AS oi
 		ON o.id_order=oi.id_order
-		LEFT JOIN calendar AS c
-		ON o.id_date=c.id_date
 		LEFT JOIN item AS i
 		ON oi.id_item=i.id_item
-GROUP BY TO_CHAR(c.date, 'month'),
-		 EXTRACT(month from c.date),
+GROUP BY TO_CHAR(o.created_at, 'month'),
+		 EXTRACT(month from o.created_at),
 		i.type
-ORDER BY  EXTRACT(month from c.date)
+ORDER BY  EXTRACT(month from o.created_at)
 ;		
-
--- 02_EDA_QUERIES/002_sales_by_sucursal.sql
+++ 02_EDA_QUERIES/002_sales_by_sucursal.sql
@@ -3,8 +3,6 @@ SELECT
 		SUM (oi.order_quantity) as quantity
 FROM orders AS o LEFT JOIN order_item AS oi
 		ON o.id_order=oi.id_order
-		LEFT JOIN calendar AS c
-		ON o.id_date=c.id_date
 		LEFT JOIN item AS i
 		ON oi.id_item=i.id_item
 		LEFT JOIN saltria_store as st
