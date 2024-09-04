ffd305ef22f6729d4ecde58bd8ca4f7c53c4b4c4
gamic <gamic.nl@gmail.com>
2024-09-04 11:52:56 -0400

    Se mejoro la data, queries y dashboard

--- 02_EDA_QUERIES/006_shift_production.sql
+++ 02_EDA_QUERIES/006_shift_production.sql
@@ -1,19 +1,22 @@
 WITH order_day AS(
-	SELECT 	o.created_at,
+	SELECT 	i.item_name, o.created_at,
 			sum(order_quantity) as order_quantity,
 			CASE
 				 WHEN EXTRACT(HOUR FROM created_at) < 10 
             	OR (EXTRACT(HOUR FROM created_at) = 10 AND EXTRACT(MINUTE FROM created_at) < 30) 
-				THEN 'morning shift'
-				ELSE 'afternoon shift'
+				THEN 'dawn shift'
+				ELSE 'morning shift'
 				END AS working_shifts
 	FROM 	order_item as oi LEFT JOIN orders as o
 			ON oi.id_order = o.id_order
-	GROUP BY o.created_at
+			LEFT JOIN item as i
+			ON i.id_item=oi.id_item
+	GROUP BY i.item_name, o.created_at
 	ORDER BY o.created_at
 )
-	SELECT working_shifts,
+	SELECT item_name, working_shifts,
 			sum(order_quantity) AS total_quantity
 	FROM	order_day
-	GROUP BY working_shifts
+	GROUP BY item_name, working_shifts
+	ORDER BY working_shifts
 	
